8000b681302f0246c1cb8b429b4c757b4193b440
Jose Gutiérrez <josegutierrezsoler@gmail.com>
2014-11-13 16:55:37 +0100

    Facebook working

--- plugins/wbb-contribution/public/class-wbb-contribution-public.php
+++ plugins/wbb-contribution/public/class-wbb-contribution-public.php
@@ -22,82 +22,177 @@
  */
 class WBB_Contribution_Public {
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $WBB_Contribution    The ID of this plugin.
-	 */
-	private $WBB_Contribution;
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $WBB_Contribution    The ID of this plugin.
+     */
+    private $WBB_Contribution;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $version    The current version of this plugin.
+     */
+    private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @var      string    $WBB_Contribution       The name of the plugin.
-	 * @var      string    $version    The version of this plugin.
-	 */
-	public function __construct( $WBB_Contribution, $version ) {
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @since    1.0.0
+     * @var      string    $WBB_Contribution       The name of the plugin.
+     * @var      string    $version    The version of this plugin.
+     */
+    public function __construct($WBB_Contribution, $version) {
 
-		$this->WBB_Contribution = $WBB_Contribution;
-		$this->version = $version;
+        $this->WBB_Contribution = $WBB_Contribution;
+        $this->version = $version;
 
-	}
+        add_action('wp_ajax_wbb_contribution_do_login', array($this, 'wbb_contribution_do_login'));
+        add_action('wp_ajax_nopriv_wbb_contribution_do_login', array($this, 'wbb_contribution_do_login'));
+    }
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
+    /**
+     * Register the stylesheets for the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in WBB_Contribution_Public_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The WBB_Contribution_Public_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in WBB_Contribution_Public_Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The WBB_Contribution_Public_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
+        wp_enqueue_style($this->WBB_Contribution, plugin_dir_url(__FILE__) . 'css/wbb-contribution-public.css', array(), $this->version, 'all');
+    }
 
-		wp_enqueue_style( $this->WBB_Contribution, plugin_dir_url( __FILE__ ) . 'css/wbb-contribution-public.css', array(), $this->version, 'all' );
+    /**
+     * Register the stylesheets for the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_scripts() {
 
-	}
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in WBB_Contribution_Public_Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The WBB_Contribution_Public_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
+        wp_enqueue_script($this->WBB_Contribution . "-hellojs", plugin_dir_url(__FILE__) . 'js/hellojs/hello.js', array('jquery'), $this->version, false);
+        wp_enqueue_script($this->WBB_Contribution . "-hellojs-then", plugin_dir_url(__FILE__) . 'js/hellojs/hello.then.js', array('jquery'), $this->version, false);
+        wp_enqueue_script($this->WBB_Contribution . "-hellojs-ids", plugin_dir_url(__FILE__) . 'js/hellojs/client_ids.js', array('jquery'), $this->version, false);
+        wp_enqueue_script($this->WBB_Contribution . "-hellojs-twitter", plugin_dir_url(__FILE__) . 'js/hellojs/modules/twitter.js', array('jquery'), $this->version, false);
+        wp_enqueue_script($this->WBB_Contribution . "-hellojs-facebook", plugin_dir_url(__FILE__) . 'js/hellojs/modules/facebook.js', array('jquery'), $this->version, false);
+        wp_enqueue_script($this->WBB_Contribution . "-wbb-contribution-public", plugin_dir_url(__FILE__) . 'js/wbb-contribution-public.js', array('jquery'), $this->version, false);
+        wp_localize_script(
+                $this->WBB_Contribution . "-wbb-contribution-public"
+                , 'MyAjax'
+                , array(
+            // URL to wp-admin/admin-ajax.php to process the request
+            'ajaxurl' => admin_url('admin-ajax.php')
+                )
+        );
+    }
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
+    public function wbb_contribution_do_login() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in WBB_Contribution_Public_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The WBB_Contribution_Public_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
 
-		wp_enqueue_script( $this->WBB_Contribution, plugin_dir_url( __FILE__ ) . 'js/wbb-contribution-public.js', array( 'jquery' ), $this->version, false );
+        if (isset($_POST["social"])) {
 
-	}
+            if ($_POST["social"] === "facebook") {
+
+                $user = $_POST["user"];
+                $this->check_facebook_user($user);
+            } else if ($_POST["social"] === "twitter") {
+
+                $user = $_POST["user"];
+                $this->check_twitter_user($user);
+            } else {
+
+                echo "<br>Desconocido, fin de línea";
+            }
+        } else {
+            echo "<br>No social";
+        }
+        die();
+    }
+
+    public function check_facebook_user($user) {
+
+
+        $user_id = get_user_by("email", $user["email"])->ID;
+
+        if ($user_id) {
+
+            $user = get_user_by('id', $user_id);
+
+            if ($user) {
+
+                wp_set_current_user($user_id, $user->user_login);
+                wp_set_auth_cookie($user_id);
+                do_action('wp_login', $user->user_login);
+            }
+        } else {
+
+            //"User Not register - Register and Login";
+            $this->register_and_login_new_user($user["name"], $user["email"]);
+        }
+    }
+
+    public function check_twitter_user($user) {
+
+        print_r("user <br>");
+        
+        //$email = $user["name"] . "@twitter_dummy_mail.com";
+
+        $user_id = get_user_by("email", $email)->ID;
+
+        if ($user_id) {
+
+            $user = get_user_by('id', $user_id);
+
+            if ($user) {
+
+                wp_set_current_user($user_id, $user->user_login);
+                wp_set_auth_cookie($user_id);
+                do_action('wp_login', $user->user_login);
+            }
+        } else {
+
+            $name = $user["name"];
+
+            //"User Not register - Register and Login";
+            $this->register_and_login_new_user($name, $email);
+        }
+    }
+
+    function register_and_login_new_user($name, $email) {
+
+        
+        
+        $password = wp_generate_password(12, true);
+        $new_user_id = wp_create_user($name, $password, $email);
+
+        print_r("register: ". $name . " / $password / $email");
+        
+        wp_set_current_user($new_user_id);
+    }
 
 }
--- themes/contribution/templates/header.php
+++ themes/contribution/templates/header.php
@@ -1,3 +1,36 @@
 <header><!---You can add your top menu template file here/--></header>
 <?php echo get_template_part ( 'templates/menu', 'top' ); ?>
 
+<script>
+    /*
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '752931978078014',
+      xfbml      : true,
+      version    : 'v2.2'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+   */
+</script>
+
+
+<button class="js-login-facebook">facebook</button>
+<button class="js-login-twitter">Twitter</button>
+
+<hr>
+
+<?php
+
+global $current_user;
+
+print_r($current_user);
+
+?>
